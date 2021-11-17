7d126449914b2f243d62f3c1c4262aa18000e83f
Abdur Rahim <akas25n@gmail.com>
2021-11-17 21:05:30 +0100

    added post test

--- src/test/java/com/restassuredapitest/examples/GetAndPostExamples.java
+++ src/test/java/com/restassuredapitest/examples/GetAndPostExamples.java
@@ -2,7 +2,13 @@ package com.restassuredapitest.examples;
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
+
+import io.restassured.http.ContentType;
+import org.json.simple.JSONObject;
 import org.testng.annotations.Test;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class GetAndPostExamples {
 
@@ -16,5 +22,28 @@ public class GetAndPostExamples {
                 .then()
                 .body("data.first_name", hasItem("Lindsay"))
                 .body("data[2].first_name", equalTo("Tobias"));
+    }
+
+    @Test
+    public void testPost(){
+
+        baseURI = "https://reqres.in/api";
+
+        Map<String, Object> map = new HashMap<>();
+
+        JSONObject request = new JSONObject(map);
+
+        request.put("name", "Akash");
+        request.put("job", "Teacher");
+
+        given()
+                .header("Content-Type", "application/json")
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .body(request.toJSONString())
+                .when()
+                    .post("/users")
+                .then()
+                    .statusCode(201).log().all();
     }
 }
